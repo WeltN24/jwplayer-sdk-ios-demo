3b0f0155e1c28dc0974fddaa28d4c1600fc6b1aa
Felix Dietz <felix.dietz@weltn24.de>
2016-09-01 14:06:31 +0200

    bar

--- JWPlayer-Developer-Demo/AppDelegate_swift.swift
+++ JWPlayer-Developer-Demo/AppDelegate_swift.swift
@@ -12,44 +12,38 @@ import Foundation
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
-  
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-    print("hi")
-    return true
-  }
-  
-  
   /*
    This method is overridden to make it more similar to our app.
    Because our app is portrait mode only we had to hack this in order to enable fullscreen for videos.
    A side effect to this is, that when no video is in the viewport, when rotating the device, the status bar still rotates but the rest stays in portrait
    
-   It is actually not needed to override this to produce the other issue, so feel free to comment this method and enable all orientations in the target if needed
+   
+   
+   It is actually not needed to override this method to produce the other issue, so feel free to comment this method and enable all orientations in the target if needed
    */
   func application(application: UIApplication, supportedInterfaceOrientationsForWindow window: UIWindow?) -> UIInterfaceOrientationMask {
     guard let topController = window?.topController() else {
       return application.supportedInterfaceOrientationsForWindow(window)
     }
     
+    // Another thing we noticed is that with every new player you create another UIWindow and these accumulate with several players on the same page
+    print("\n\n\(window)")
+    
     let className = NSStringFromClass(topController.dynamicType)
     
-    print("\n\n\(window)")
     if className.lowercaseString == "jwfullscreenviewcontroller" {
-      print("\n\n\(topController.description)")
-      print("\(topController.view.frame)")
       
       return .AllButUpsideDown
     }
     return application.supportedInterfaceOrientationsForWindow(window)
   }
-  
 }
 
 public extension UIWindow {
-  /*
+  /**
    Depicts the topmost view controller by recursively getting the presentedViewController in the hierarchy.
    
-   :returns: The topmost view controller
+   - returns: The topmost view controller
    */
   func topController() -> UIViewController? {
     var result = self.rootViewController
--- JWPlayer-Developer-Demo/SwiftPlayerViewController.swift
+++ JWPlayer-Developer-Demo/SwiftPlayerViewController.swift
@@ -17,23 +17,26 @@ class SwiftPlayerViewController: UITableViewController {
   private var firstplayer:JWPlayerController!
   private var secondPLayer: JWPlayerController!
   
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-  }
   
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-  }
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.navigationItem.title = "Swift Implementation"
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-  }
-  
+  /*
+   This setup makes sure we have some scrolling space between the two videos.
+   
+   What's the issue:
+   When the second video has been initialized and we scroll back to the first and go into landscape mode by rotating, the second video is shown, not the first one
+   
+   Steps to reproduce:
+   - start in portrait mode
+   
+   - play first video for a while
+   - pause first video
+   
+   - scroll to second video
+   - play second video for a while
+   - pause second video
+   
+   - scroll back to first video
+   - rotate phone into landscape
+   */
   override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
     if indexPath.section == 0 && firstplayer == nil {
       let firstConfig = JWConfig(contentURL: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
@@ -43,11 +46,13 @@ class SwiftPlayerViewController: UITableViewController {
       
       firstplayer.forceFullScreenOnLandscape = true
       firstplayer.forceLandscapeOnFullScreen = true
+      
     } else if indexPath.section == 2 && secondPLayer == nil {
       let secondConfig = JWConfig(contentURL: "http://thedigitaltheater.com/wp-content/uploads/2015/08/thx_eclipse_long-1080p_AC3_5.1_thedigitaltheater.mp4")
       secondPLayer = JWPlayerController(config: secondConfig)
       secondVideoContainer.addSubview(secondPLayer.view)
       secondPLayer.view.frame = secondVideoContainer.bounds
+      
       secondPLayer.forceFullScreenOnLandscape = true
       secondPLayer.forceLandscapeOnFullScreen = true
     }
